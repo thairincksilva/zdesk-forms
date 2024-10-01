2bdbd3e0fc44b5cf0a42a7d216a8290dfdc91ba6
Thairinck da Silva <thairinck.ap@gmail.com>
2024-10-01 13:40:59 -0300

    correção em busca de contato

--- api/getContactByEmail.js
+++ api/getContactByEmail.js
@@ -6,6 +6,7 @@ const orgId = '861735330';
 export default async function getContactByEmail(req, res) {
   const email = req.query.email;
 
+  // Obter novo access token
   const accessToken = await getNewAccessToken(); 
   if (!accessToken) {
     res.status(500).json({ message: 'Erro ao obter access token' });
@@ -13,6 +14,7 @@ export default async function getContactByEmail(req, res) {
   }
 
   try {
+    // Fazer a requisição para buscar o contato pelo e-mail
     const response = await fetch(`https://desk.zoho.com/api/v1/contacts/search?email=${email}`, {
       method: 'GET',
       headers: {
@@ -23,8 +25,10 @@ export default async function getContactByEmail(req, res) {
     });
 
     const data = await response.json();
-    if (response.ok && data.length > 0) {
-      res.status(200).json(data); // Retorna os dados do contato encontrado
+
+    // Verificar se a resposta foi bem-sucedida e se há contatos encontrados
+    if (response.ok && data.data && data.data.length > 0) {
+      res.status(200).json(data.data[0]); // Retorna o primeiro contato encontrado
     } else {
       res.status(404).json({ message: 'Contato não encontrado', data });
     }
