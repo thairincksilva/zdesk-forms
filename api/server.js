cf58a05530e5838b2532478f6b50fe62924a2add
Thairinck <thairinck.ap@gmail.com>
2024-10-03 07:18:18 -0300

    return2

--- api/server.js
+++ api/server.js
@@ -4,17 +4,10 @@ import cors from 'cors';
 import createTicket from './createTicket.js'; 
 import createContact from './createContact.js'; 
 import getCustomer from './getCustomer.js';
-import getContactByEmail from './getContactByEmail.js';  
-import multer from 'multer'; // Importando multer
-import fs from 'fs'; 
-import path from 'path'; 
-import { attachFileToTicket } from './createTicketAttachment.js'; // Importando a função para anexar arquivos
+import getContactByEmail from './getContactByEmail.js';  // Importe a funcionalidade de buscar contato
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Configuração do multer
-const upload = multer({ dest: 'api/uploads/' }); // O multer irá salvar os arquivos temporariamente nesta pasta
 
 // Middleware
 app.use(cors());
@@ -58,31 +51,19 @@ app.get('/api/getCustomer/:id', async (req, res) => {
   }
 });
 
+// // Rota para buscar contato pelo email
+// app.get('/api/getContactByEmail', async (req, res) => {
+//   try {
+//     await getContactByEmail(req, res);  // Utilize a função de buscar contato que foi criada no arquivo dedicado
+//   } catch (error) {
+//     console.error('Erro ao buscar contato:', error);
+//     res.status(500).json({ message: 'Erro interno ao buscar contato' });
+//   }
+// });
+
 // Rota para buscar contato pelo email
 app.get('/api/getContactByEmail', async (req, res) => {
   await getContactByEmail(req, res);  // Certifique-se de que essa função está implementada corretamente
-});
-
-// Nova rota para anexar arquivo a um ticket
-app.post('/api/tickets/:ticketId/attachments', upload.single('file'), async (req, res) => {
-  const ticketId = req.params.ticketId; // Pega o ID do ticket a partir da rota
-  const filePath = req.file.path; // Caminho temporário onde o multer salvou o arquivo
-
-  if (!req.file) {
-    return res.status(400).json({ message: 'Nenhum arquivo enviado.' });
-  }
-
-  try {
-    // Chamando a função de anexar arquivo a partir de createTicketAttachment.js
-    const response = await attachFileToTicket(ticketId, filePath);
-    res.status(200).json({ message: 'Anexo criado com sucesso!', data: response });
-  } catch (error) {
-    console.error('Erro ao anexar arquivo ao ticket:', error);
-    res.status(500).json({ message: 'Erro interno ao anexar arquivo ao ticket' });
-  } finally {
-    // Remover o arquivo após o envio
-    fs.unlinkSync(filePath);
-  }
 });
 
 // Iniciar o servidor
