8dd9c3c22c4d98710ca9820a681fa7e04d860a74
Thairinck da Silva <thairinck.ap@gmail.com>
2024-10-06 23:32:31 -0300

    update

--- api/middleware/handleFilleMiddleware.js
+++ api/middleware/handleFilleMiddleware.js
@@ -2,34 +2,42 @@ import busboy from 'busboy';
 
 export function fileUploadMiddleware(request, response, next) {
     const busboyHandle = busboy({
-        headers: request.headers,
-    });
-
-    request.body = {}; 
-    request.files = {};
-
-
-
-
-    busboyHandle.on('field', (name, value) => {
-        request.body[name] = value;
-    });
-
-    busboyHandle.on('file', (inputName, stream, info) => {
-        request.files[inputName] = {
-            ...info,
-            stream,
-            inputName,
-        };
-
-        stream.on('error', (err) => {
-            next(new Error('File stream error: ' + err.message));
-        });
-    });
-
-    busboyHandle.on('finish', () => {
-        next(); 
-    });
+        headers: request.headers
+      })
+    
+      busboyHandle.on('field', (name, value) => {
+        request.body[name] = value
+      })
+    
+      busboyHandle.on(
+        'file',
+        (inputName, stream, info) => {
+          stream.on('data', (buffer) => {
+            if (request.files === undefined) {
+              request.files = {}
+            }
+    
+            request.files[inputName] = {
+              ...info,
+              stream,
+              inputName,
+              buffer
+            }
+          })
+    
+          stream.on('error', () => {
+            throw new ValidationException(
+              __('messages.validation.maxFileLength', {
+                length: '4 Megabytes'
+              })
+            )
+          })
+        }
+      )
+    
+      busboyHandle.on('finish', () => {
+        next()
+      })
 }
 
 export default fileUploadMiddleware;
