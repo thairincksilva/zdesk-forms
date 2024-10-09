d08c3d77a2dd1386ee76756d2dbfb65fc98db2c6
Thairinck da Silva <thairinck.ap@gmail.com>
2024-10-09 10:53:18 -0300

    multer formdata 2

--- api/createTicketAttachment.js
+++ api/createTicketAttachment.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import FormData from 'form-data';
 import fs from 'fs';
-import { createFile } from './createFile';
+import { createFile } from './createFile.js';
 
 const clientId = '1000.KC7V3888M7W0M0BYHDFA6ORJV1082L';
 const clientSecret = '6686ff0d6d0c7a0a56c46af8daf360cb3700c05852';
